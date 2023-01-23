e527b9f37be6db4d15c556b1b5d8f1e278cc265c
Morgan Waddington <waddington.morgan@gmail.com>
2023-01-23 17:45:08 -0500

    my any?

--- enumerables.rb
+++ enumerables.rb
@@ -12,27 +12,46 @@ class Array
 
 
     def my_select(&block)
-        
+        farr = []
+        self.my_each do |el|
+           farr << el if block.call(el)
+        end
+        farr
+    end
+
+    def my_reject(&block)
+        farr = []
+        self.my_each do |el|
+           farr << el if !block.call(el)
+        end
+        farr
     end
 
 
 end
+# a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
 
 
-
-# calls my_each twice on the array, printing all the numbers twice.
-return_value = [1, 2, 3].my_each do |num|
-    puts num
-    end
-    [1, 2, 3].my_each do |num|
-    puts num
-    end
-    # # => 1
-    #     2
-    #     3
-    #     1
-    #     2
-    #     3
+# # calls my_each twice on the array, printing all the numbers twice.
+# return_value = [1, 2, 3].my_each do |num|
+#     puts num
+#     end
+#     [1, 2, 3].my_each do |num|
+#     puts num
+#     end
+#     # # => 1
+#     #     2
+#     #     3
+#     #     1
+#     #     2
+#     #     3
     
-    p return_value  # => [1, 2, 3]
+#     p return_value  # => [1, 2, 3]
 
+
+
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
